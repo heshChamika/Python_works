a691620bcbc998d32431e1644390570d8d5f2545
heshChamika <127346128+heshChamika@users.noreply.github.com>
2023-09-16 22:10:55 +0530

    Add files via upload

---
+++ project_d.py
@@ -0,0 +1,14 @@
+
+sum_of_numbers = 0
+input_number = 0
+
+
+while input_number != -999:
+    input_number = int(input("Enter a number: "))
+    sum_of_numbers += input_number
+
+#
+sum_of_numbers -= 999
+
+
+print("Sum of the entered numbers:", sum_of_numbers)
