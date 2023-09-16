4d483b0d883c7f4d2766defff30d0c7dbaeae220
heshChamika <127346128+heshChamika@users.noreply.github.com>
2023-09-16 22:09:59 +0530

    Add files via upload

---
+++ project_b.py
@@ -0,0 +1,19 @@
+St1 = int(input("Enter Your marks: "))
+St2 = int(input("Enter Your marks: "))
+St3 = int(input("Enter Your marks: "))
+St4 = int(input("Enter Your marks: "))
+St5 = int(input("Enter Your marks: "))
+St6 = int(input("Enter Your marks: "))
+St7 = int(input("Enter Your marks: "))
+St8 = int(input("Enter Your marks: "))
+St9 = int(input("Enter Your marks: "))
+St10 = int(input("Enter Your marks: "))
+
+tot = St1+St2+St3+St4+St5+St6+St7+St8+St9+St10
+avg = tot/10
+minimum = min(St1,St2,St3,St4,St5,St6,St7,St8,St9,St10)
+maximum = max(St1,St2,St3,St4,St5,St6,St7,St8,St9,St10)
+
+print(avg)
+print(minimum)
+print(maximum)
